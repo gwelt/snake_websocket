1fd269e9bc8c4f3c6a61189e92fc6b408a8966da
Sven <sven@gwelt.net>
2016-12-20 20:36:55 +0100

    first commit

---
+++ port3000redirect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#sudo service apache2 stop
+sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 3000
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
+sudo iptables -t nat -L --line-numbers
+echo -e "\nNOW REDIRECTING INCOMING/DESTINATION \e[42mPORT 80\e[49m TO \e[42mPORT 3000\e[49m"
---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+const debug=true;
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, function() {
+    console.log(`\x1bcSNAKE SERVER LISTENING ON PORT ${ PORT }`);
+  });
+const wss = new SocketServer({ server });
+var players={};
+
+function new_player() {
+  var list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return list.charAt(Math.floor(Math.random() * list.length));
+}
+
+function log(text,col) {
+  if (debug) {if (col>0) {process.stdout.write('\x1b['+col+'m '+text+' \x1b[0m ')} else {process.stdout.write(text+' ');}} return;
+}
+
+function wss_send_to_all_players(msg) {wss.clients.forEach((client) => {client.send(msg)})}
+
+wss.on('connection', (ws) => {
+  var player=new_player();
+  log('WELCOME '+player,42); ws.send('WELCOME PLAYER '+player);
+  ws.on('close', () => log('BYE-BYE '+player,41));
+  ws.on('message', (msg) => {
+    //if (msg.startsWith('HELLO')) {log('HELLO PLAYER '+player,42); msg='HELLO PLAYER '+player;}
+    log(player+msg,0);
+    wss_send_to_all_players(player+msg);
+    if (msg=='Q') {ws.send('BYE-BYE!'); ws.close();} 
+  });
+});
+
+/*
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
+*/
